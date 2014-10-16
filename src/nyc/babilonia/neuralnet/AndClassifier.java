11d04dc6bbe2074e3934d15ca59cb28ebcc4711e
fbabilonia <fbabilonia@gmail.com>
2014-10-16 00:13:39 -0400

    AndClassifier
    finished, tested and successfully classify's AND logic function.

--- src/nyc/babilonia/neuralnet/AndClassifier.java
+++ src/nyc/babilonia/neuralnet/AndClassifier.java
@@ -2,21 +2,48 @@ package nyc.babilonia.neuralnet;
 
 public class AndClassifier
 {
-	Neuron perceptron;
+	static Neuron perceptron = new Neuron(2,.1,0,null,new ThresholdFunction());
 	//just initializes the Neuron.
 	public AndClassifier()
 	{
-		perceptron = new Neuron(2,.1,0,null,new ThresholdFunction());
-	}
-	public static void train(double [][] data)
-	{
-		
 	}
 	
+	public static void train(double [][] data)
+	{
+		double error;
+		do
+		{
+			error = 0;
+			for(double [] input : data)
+			{
+				send(input);
+				error += Math.abs((input[2] - perceptron.getOutput()));
+				perceptron.updateWeights(input[2]);
+			}
+			System.out.println(error);
+		}
+		while (error >0);
+	}
+
+	private static void send(double[] input)
+	{
+		perceptron.setInput(0, input[0]);
+		perceptron.setInput(1, input[1]);
+		perceptron.activate();
+	}
+	public static boolean test(double [] test)
+	{
+		send(test);
+		return perceptron.getOutput() == test[2];
+	}
 	public static void main(String args[])
 	{
 		double [][] data ={ {0,0,0},
 				{0,1,0} , {1,0,0} , {1,1,1}};
 		train(data);
+		for(double [] test : data)
+		{
+			System.out.println(test(test));
+		}
 	}
 }
